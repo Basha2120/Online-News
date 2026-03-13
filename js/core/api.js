69eb26d9d73aed3d16b4cde9ebda11e4513692c1
Basharathullah <bashara2272@gmail.com>
2026-03-14 00:13:49 +0530

    Deployment Execution

--- js/core/api.js
+++ js/core/api.js
@@ -1,4 +1,6 @@
-const API_BASE = "http://localhost:3000";
+const API_BASE = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+  ? "http://localhost:3000"
+  : "https://online-news-backend.onrender.com"; // We will rename this once you have your actual Render URL
 
 export function getToken() {
   return localStorage.getItem("authToken");
